86ff5875b57aed8ca9a00ebe08ae288fdcad3836
Donatas Kairys <donataskairys@Donatass-MacBook-Pro-2.local>
2022-02-08 11:09:59 -0600

    Restructed AuthProvider

--- src/Features/Auth/AuthProvider.tsx
+++ src/Features/Auth/AuthProvider.tsx
@@ -20,7 +20,6 @@ export interface AuthProviderData {
   web3auth: Web3Auth,
   provider: CONNECTED_EVENT_DATA,
   user: Partial<UserInfo>,
-  onInit: (web3Auth: Web3Auth) => void,
   onSuccessfulLogin: (data: CONNECTED_EVENT_DATA, user: any) => void,
   login: () => void,
   logout: () => void,
@@ -30,22 +29,21 @@ export const AuthProviderContext = React.createContext<AuthProviderData>({
   web3auth: null,
   provider: null,
   user: null,
-  onInit: (web3Auth: Web3Auth) => {},
   onSuccessfulLogin: (data: any) => {},
   login: () => {},
   logout: () => {},
 })
 
+const web3auth = new Web3Auth({
+  chainConfig: solanaChainConfig,
+  clientId: WEB3AUTH_CLIENT_ID // get your clientId from https://developer.web3auth.io
+});
+
 export const AuthProvider: React.FC = ({
   children,
 }) => {
-  const [web3auth, setWeb3Auth] = useState<Web3Auth>(null)
   const [provider, setProvider] = useState<CONNECTED_EVENT_DATA>(null)
   const [user, setUser] = useState<Partial<UserInfo>>(null)
-
-  const onInit = useCallback((web3auth: Web3Auth) => {
-    setWeb3Auth(web3auth)
-  }, [])
 
   const onSuccessfulLogin = useCallback((data: CONNECTED_EVENT_DATA, user: Partial<UserInfo>) => {
     console.log('onSuccessfulLogin', data, user)
@@ -59,16 +57,15 @@ export const AuthProvider: React.FC = ({
     }).catch(err => {
       console.log(err)
     })
-  }, [web3auth])
+  }, [])
 
   const logout = useCallback(() => {
     web3auth.logout().then(() => {
-      setUser(null)
-      setProvider(null)
+      // login on logout
     }).catch(err => {
       console.log('logout', err)
     })
-  }, [web3auth])
+  }, [])
 
   const subscribeAuthEvents = useCallback((web3auth: Web3Auth) => {
     web3auth.on(ADAPTER_EVENTS.CONNECTED, (data: CONNECTED_EVENT_DATA) => {
@@ -84,6 +81,8 @@ export const AuthProvider: React.FC = ({
 
     web3auth.on(ADAPTER_EVENTS.DISCONNECTED, () => {
       console.log("disconnected");
+      setUser(null)
+      setProvider(null)
     });
 
     web3auth.on(ADAPTER_EVENTS.ERRORED, (error) => {
@@ -96,25 +95,18 @@ export const AuthProvider: React.FC = ({
   }, [onSuccessfulLogin])
 
   useEffect(() => {
-    const web3auth = new Web3Auth({
-      chainConfig: solanaChainConfig,
-      clientId: WEB3AUTH_CLIENT_ID // get your clientId from https://developer.web3auth.io
-    });
-    setWeb3Auth(web3auth)
-
     subscribeAuthEvents(web3auth)
 
     web3auth.initModal()
       .catch(err => {
         alert('error' + err)
       })
-  }, [subscribeAuthEvents])
+  }, [])
 
   const ctx: AuthProviderData = {
     web3auth,
     provider,
     user,
-    onInit,
     onSuccessfulLogin,
     login,
     logout,
